36c81b7a3b16e88685d06d891b8b69e4011bc244
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-03-11 19:21:16 +0530

    shortest encapsultor brute force

---
+++ code_problem_1.py
@@ -0,0 +1,40 @@
+# input: line 1 gives the count of strings to follow
+# N lines follow where each line has 1 string
+
+def shortest_encapsulator(S):
+    if len(S) == 1:
+        return S[0]
+
+    result = S[0]
+    for k in range(1, len(S)):
+        i = 0
+        l = len(S[k])
+        candidates = []
+        for i in range(l):
+            candidates.append(S[k][0:l - i])
+
+        j = 0
+        while j < len(candidates):
+            if candidates[j] == result[-len(candidates[j]):]:
+                break
+            else:
+                j += 1
+        if j < len(candidates):
+            result += S[k][len(candidates[j]):]
+    return result
+
+if __name__ == "__main__":
+    S = []
+    count = int(input())
+    for _ in range(count):
+        S.append(input())
+
+    expected_out = "abracadabracad"
+    se = shortest_encapsulator(S)
+    print(f"output string: {se}")
+    print(f"Expected Output : {expected_out}")
+    if expected_out == se:
+        print("Passed")
+    else:
+        print("Failed")
+    
