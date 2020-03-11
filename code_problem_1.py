6f41804ec40173d52c36550da081396bb7e322e5
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-03-11 19:59:52 +0530

    rm string concat

--- code_problem_1.py
+++ code_problem_1.py
@@ -5,7 +5,8 @@ def shortest_encapsulator(S):
     if len(S) == 1:
         return S[0]
 
-    result = S[0]
+    result = []
+    result.append(S[0])
     for k in range(1, len(S)):
         i = 0
         l = len(S[k])
@@ -15,13 +16,16 @@ def shortest_encapsulator(S):
 
         j = 0
         while j < len(candidates):
-            if candidates[j] == result[-len(candidates[j]):]:
+            if candidates[j] ==''.join(result)[-len(candidates[j]):]:
                 break
             else:
                 j += 1
         if j < len(candidates):
-            result += S[k][len(candidates[j]):]
-    return result
+            result.append(S[k][len(candidates[j]):])
+
+    final_str = ''.join(result)
+    return final_str
+
 
 if __name__ == "__main__":
     S = []
