f6041a4668603a36761fdb988e02394942f506c8
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-03-12 23:54:50 +0530

    lcs improvement brute force

--- lcs_1.py
+++ lcs_1.py
@@ -1,15 +1,25 @@
 def lcs(S):
     if len(S) < 2:
         return S[0]
-    
-    s1 = S[0]
+
+    candidate_strs = [S[0][i:] for i in range(len(S[0]))]
+    lcs_strs = []
     s2 = S[1]
+    for s1 in candidate_strs:
+        s = get_lcs(s1, s2)
+        lcs_strs.append(s)
+
+    lcs = get_global_lcs(lcs_strs)
+    return lcs
+
+
+def get_lcs(s1, s2):
     result = []
     i = 0
     pos = -1
     while i < len(s1) and pos < len(s2):
         new_pos = get_pos(s1[i], s2[pos + 1:])
-        print(i, new_pos, pos)
+
         if new_pos >= 0:
             pos = pos + new_pos + 1
             result.append(s1[i])
@@ -17,6 +27,20 @@ def lcs(S):
         
     lcs_str = ''.join(result)
     return lcs_str
+
+
+def get_global_lcs(lcs_strs):
+    i = 0
+    max_len = 0
+    max_idx = 0
+    while i < len(lcs_strs):
+        if len(lcs_strs[i]) > max_len:
+            max_len = len(lcs_strs[i])
+            max_idx = i
+        i += 1
+        
+    return lcs_strs[max_idx]
+
 
 def get_pos(c, target_str):
     pos = -1
@@ -39,4 +63,3 @@ if __name__ == "__main__":
     print(f"input strings: {S}")
     s = lcs(S)
     print(f"lcs: {s}")
-
