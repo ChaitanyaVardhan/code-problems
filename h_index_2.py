0d0fb9227942d7ec2674cf26664e2b819da7b338
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-03-15 15:07:52 +0530

    h index in O(n^2) time

---
+++ h_index_2.py
@@ -0,0 +1,45 @@
+def compute_h_index(S):
+    result = []
+    for i in range(1, len(S)+1):
+        h = compute_h_index_for_set(S[0:i])
+        result.append(h)
+
+    return result
+
+
+def compute_h_index_for_set(X):
+    citation_arr = [0] * len(X)
+    for x in X:
+        if x < len(X):
+            citation_arr[x-1] += 1
+        else:
+            citation_arr[len(X)-1] += 1
+    print(f"citation_arr = {citation_arr}")
+            
+    score_arr = [0] * len(X)
+    total_sum = 0
+    for j in range(len(X)):
+        total_sum += citation_arr[j]
+    score_arr[len(X)-1] = citation_arr[len(X)-1]
+    l = len(X) - 2
+    while l >= 0:
+        score_arr[l] = citation_arr[l] + citation_arr[l+1]
+        l -= 1
+
+    print(f"score_arr = {score_arr}")
+    h_index = 0
+    for l in range(len(X)):
+        if score_arr[l] >= l + 1:
+            h_index = l + 1
+
+    return h_index
+
+
+if __name__ == "__main__":
+    T = int(input())
+    for t in range(T):
+        N = int(input())
+        S = [None] * N
+        S = [int(i) for i in input().split(" ")]
+        result = compute_h_index(S)
+        print("Case #{}: {}".format(t+1, ' '.join(str(h) for h in result)))
