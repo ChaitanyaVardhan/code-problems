1333f81752ed7e4c9934e7ee34a867d3c4438fcb
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-03-22 17:31:38 +0530

    inefficient integer solutions

---
+++ integer_solutions.py
@@ -0,0 +1,33 @@
+def compute_integer_solutions(N, K):
+    if N < 2:
+        S = list()
+        for i in range(K):
+            s = [0] * K
+            s[i] = 1
+            S.append(s)
+        return S
+
+    S = compute_integer_solutions(N-1, K)
+
+    A = merge_one(S)
+
+    return A
+
+
+def merge_one(S):
+    A = list()
+    for s in S:
+        for j in range(K):
+            a = list(s)
+            a[j] += 1
+            A.append(a)
+    return A
+    
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        N, K = [int(s) for s in input().split(" ")]
+        r = compute_integer_solutions(N, K)
+        print(r)
+    
